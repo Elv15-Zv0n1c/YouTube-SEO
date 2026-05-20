aea23496be38b05939e84bbf97071bf2c38798ed
Elv15-Zv0n1c <elvis.zvonic@gmail.com>
2026-05-20 22:21:40 +0200

    Update generate-metadata.ts

--- api/generate-metadata.ts
+++ api/generate-metadata.ts
@@ -22,7 +22,7 @@ export default async function handler(req, res) {
     });
 
     return res.status(200).json({
-      result: response.text,
+      result: response.text(),
     });
 
   } catch (error) {
