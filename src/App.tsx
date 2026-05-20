869d8a41a17af40a3f9c4411f3796b444f0cd057
Elv15-Zv0n1c <elvis.zvonic@gmail.com>
2026-05-21 00:27:23 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,3 @@
-import { useEffect, useState } from "react";
-
 export default function App() {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    console.log("App loaded");
-  }, []);
-
-  return (
-    <div style={{ padding: 20 }}>
-      <h1>App läuft</h1>
-    </div>
-  );
+  return <div style={{ padding: 20 }}>RESET OK</div>;
 }
